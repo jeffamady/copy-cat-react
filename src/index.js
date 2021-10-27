9a82b0a6aa78b730615b19d61ab885e721160163
jeffamady <jeff02.jn@gmail.com>
2021-10-27 20:21:25 -0300

    copy cat project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import CopyCatContainer from './containers/CopyCatContainer';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CopyCatContainer />
   </React.StrictMode>,
   document.getElementById('root')
 );
